3dc546751a20c1dfe67290f2c3d351f3090b2905
AugustinCms <augustin.camus@epitech.eu>
2023-12-07 16:54:13 +0100

    feat: the data file is re-upload every tuesday at 3 AM

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-// basic express server
-
 const express = require('express');
 const app = express();
 const port = 3000;
 const getDataGouv = require('./getDataGouv.js');
 const bodyParser = require('body-parser');
+const cron = require('node-cron');
 const { parseDataLocalisation, parseData } = require('./parseData.js');
 let dataLocalisation = [];
 let data;
+
+cron.schedule('0 3 * * 2', async () => {
+  await getDataGouv();
+  dataLocalisation = parseDataLocalisation();
+  console.log('data updated');
+}
+);
 
 async function initDataGouv() {
   await getDataGouv();
@@ -22,7 +28,6 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/dataLocalisation', (req, res) => {
-  console.log(dataLocalisation);
   res.send(dataLocalisation);
 });
 
@@ -31,4 +36,4 @@ app.post('/data', async (req, res) => {
   res.send(data);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port);
--- src/parseData.js
+++ src/parseData.js
@@ -28,18 +28,12 @@ function parseDataLocalisation() {
         } else {
           latitude = coordinates[1].substring(0, coordinates[1].length - 1);
         }
-        // outputData.some((item) => {
-        //   console.log(item.longitude, longitude)
-        // });
-        // if coordinates are not already in the outputData array, add them
         if (!outputData.some((item) => item.longitude === parseFloat(longitude) && item.latitude === parseFloat(latitude))) {
           outputData.push({ longitude: parseFloat(longitude), latitude: parseFloat(latitude) });
           mainData.push(feature);
         }
       }
     }
-    fs.writeFileSync('output.json', JSON.stringify(outputData, null, 2));
-    console.log('Data extracted and saved to output.json');
   });
   return outputData;
 }
